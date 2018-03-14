6d11200e91b99e6aeb7428bd55034199bd706c11
Sarah Speck <smspeck13@gmail.com>
2018-03-14 00:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var a,b
 
-function add () {
-  (a+b)
+function add (a,b) {
+  return a+b
 }
 
-function subtract () {
-  (a-b)
+function subtract (a,b) {
+  return a-b
 }
 
 function multiply () {
