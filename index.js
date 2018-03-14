e53ceb9ad57ede927ee126adc3f4c8e6f7ee19bd
Sarah Speck <smspeck13@gmail.com>
2018-03-14 00:27:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var a,b 
+
+function add () {
+  a,b a+b   
+}
+
+function subtract () {  
+}
+
+function multiply () { 
+}
+
+function divide () {
+}
+
+function makeInt (n) {
+}
+
+function preserveDecimal (n) { 
+}
